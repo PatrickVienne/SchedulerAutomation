fa92c3bba3d66a02cdacc216aff2b832f0ad9df0
Wolfgang Jacobi <wolfgang-jacobi@gmx.net>
2016-12-15 16:54:00 +0100

    changed to my port.

--- factory.py
+++ factory.py
@@ -18,7 +18,7 @@ def create_app():
     app = Flask(__name__)
     config_name = os.getenv('FLASK_CONFIGURATION', 'default')
     app.config.from_object(CONFIG[config_name])
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@127.0.0.1:3307/scheduler'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@127.0.0.1:3306/scheduler'
     app.secret_key = app.config['SECRET_KEY']
     utilities.setup_logger()
     db.init_app(app)
